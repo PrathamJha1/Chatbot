52be1222e7e0b45f2b69406fa2b1fcd3de660fc4
jhapratham <pratham@near.com>
2023-08-19 16:26:51 +0530

    chatbot created using streamlit

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ st.set_page_config(page_title="AI Chatbot")
 with st.sidebar:
     st.title('Chatbot')
     if 'REPLICATE_API_TOKEN' in st.secrets:
-        st.toast('Replicate Model Connected!', icon='😍')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
     else:
-        st.toast("Error in connection")
+        st.error("Error in connection")
     # Refactored from https://github.com/a16z-infra/llama2-chatbot
     st.subheader('Models and parameters')
     selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B', 'Llama2-70B'], key='selected_model')
@@ -23,9 +22,6 @@ with st.sidebar:
     else:
         llm = 'replicate/llama70b-v2-chat:e951f18578850b652510200860fc4ea62b3b16fac280f83ff32282f87bbd2e48'
     
-    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
-    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider('max_length', min_value=64, max_value=4096, value=512, step=8)
     
 os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
@@ -52,7 +48,7 @@ def generate_llama2_response(prompt_input):
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
     output = replicate.run(llm, 
                            input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
+                                  "repetition_penalty":1})
     return output
 
 # User-provided prompt
